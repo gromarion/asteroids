06f8e8253ff108061f182b35f20ba21e8de62757
Facundo <fmenzella@monits.com>
2015-08-18 19:12:25 -0300

    basic menu scene added to project

---
+++ Assets/scripts/Router.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Router : MonoBehaviour {
+
+	public void showGameScene () {
+		Debug.Log ("click");
+		Application.LoadLevel("gameplay");
+	}
+}
