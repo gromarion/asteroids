faece1b2942b98036bcf156644f8dfceb0e69cc4
German Rodrigo Romarion <grromarion90@gmail.com>
2015-08-19 08:53:56 -0300

    Enhanced BoundsController. No more glitching on edge cases.

--- Assets/scripts/BoundsController.cs
+++ Assets/scripts/BoundsController.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class BoundsController : MonoBehaviour {
 
+	public const float MAP_SIZE = 5f;
+
 	public void Update () {
-		Vector3 viewport_point = Camera.main.WorldToViewportPoint(transform.position);
-		Vector3 new_position = transform.position;
-		if (viewport_point.x > 1 || viewport_point.x < 0) {
-			new_position.x = - new_position.x;
-		}
-		if (viewport_point.y > 1 || viewport_point.y < 0) {
-			new_position.y = - new_position.y;
-		}
-		transform.position = new_position;
+//		Vector3 viewport_point = Camera.main.WorldToViewportPoint(transform.position);
+		Vector3 modelPosition = transform.position * .5f + Vector3.one * .5f * MAP_SIZE;
+
+		modelPosition.x = Mathf.Repeat(modelPosition.x, MAP_SIZE);
+		modelPosition.y = Mathf.Repeat(modelPosition.y, MAP_SIZE);
+
+		transform.position = modelPosition * 2f - Vector3.one * MAP_SIZE;
 	}
 }
