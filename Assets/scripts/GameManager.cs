b7a057dd120cbd4dfdc6dca642e163a2dc3c3813
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-08-22 12:45:25 -0300

    Prevented asteroids from spawning too close to the player.

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,7 +5,9 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
 
-	public int initialAsteroids = 3;
+	public int initialAsteroids = 100;
+	public float asteroidsSpawnMinRadius = 3f;
+	public float asteroidsSpawnMaxRadius = 6f;
 	public PlayerController player;
 
 	private int remainingAsteroids = 0; // Amount of SMALL asteroids remaining in the current level
@@ -31,13 +33,30 @@ public class GameManager : MonoBehaviour {
 
 	public void CreateLevel(int level) {
 		remainingAsteroids = 0;
-		for (int i = 0; i < level + 2; i++) {
+		for (int i = 0; i < level + initialAsteroids; i++) {
 			GameObject asteroid = PoolManager.instance.asteroidPool.GetObject();
-			asteroid.SetActive(true);
-			asteroid.transform.position = Random.insideUnitCircle * 3;
-			AsteroidController asteroidController = asteroid.GetComponent<AsteroidController>();
-			asteroidController.MakeLargeAsteroid();
-			remainingAsteroids += 3 * 3 * 3;
+			if (asteroid != null) {
+				AsteroidController asteroidController = asteroid.GetComponent<AsteroidController>();
+				if (asteroidController != null) {
+					asteroid.SetActive(true);
+
+					// Make sure the asteroid doesn't spawn too close to the player
+					float rotate = Random.Range(0, 360f);
+					float dist = Random.Range(asteroidsSpawnMinRadius, asteroidsSpawnMaxRadius);
+					asteroid.transform.position = player.transform.position;
+					asteroid.transform.Rotate(new Vector3(0, 0, Random.Range(0, 360f)));
+					asteroid.transform.Translate(new Vector3(dist, dist));
+
+					asteroidController.MakeLargeAsteroid();
+					remainingAsteroids += 3 * 3 * 3;
+				}
+				else {
+					Debug.LogWarning("Asteroid pool not returning asteroids");
+				}
+			}
+			else {
+				Debug.Log("Empty asteroid pool");
+			}
 		}
 	}
 
@@ -45,7 +64,6 @@ public class GameManager : MonoBehaviour {
 		remainingAsteroids--;
 
 		if (remainingAsteroids <= 0) {
-			Debug.Log("Holy shit");
 			CreateLevel(++currentLevel);
 		}
 	}
