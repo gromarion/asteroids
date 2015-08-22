1d15a5032238ff9aba417d1b0511145c5f49fbeb
German Rodrigo Romarion <grromarion90@gmail.com>
2015-08-22 11:42:40 -0300

    Redirect to main menu when no more asteroids with GameController.

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] asteroids = GameObject.FindGameObjectsWithTag("asteroid");
+		
+		if (asteroids.Length == 0) {
+			Application.LoadLevel("mainmenu");
+		}
+	}
+}
