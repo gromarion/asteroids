282b857753f66055d940f50bfb099030dc291176
Facundo <fmenzella@monits.com>
2015-08-25 17:20:04 -0300

    pause working but cannot play again.

--- Assets/scripts/PauseDelegate.cs
+++ Assets/scripts/PauseDelegate.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pause : MonoBehaviour {
+public class PauseDelegate : MonoBehaviour {
 
 	public bool pause;
+	public GameObject pnlPause;
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.P)){
+		if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape)){
 			pause = !pause;
 			if (pause) {
+				pnlPause.SetActive(true);
 				Time.timeScale = 0;
 			} else {
+				pnlPause.SetActive(false);
 				Time.timeScale = 1;
 			}
 		}
